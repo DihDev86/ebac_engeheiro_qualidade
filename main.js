ff44ca3f1b5204cfe2dbefef4b4add70ff0c729c
Diego Ernandes <diegobass405@gmail.com>
2025-03-19 16:23:35 -0300

    Exercicio HTML

---
+++ main.js
@@ -0,0 +1,25 @@
+let nota = 7
+;
+let status = "Reprovado";
+
+nota >= 7 && (status = "Aprovado") 
+
+console.log(status)
+
+//---------------------------------------------------------
+
+const nome = "Diêgo";
+
+nome.length > 0 && console.log(nome)
+
+//------------------------------------------------------------
+
+const nome2 = "Talita";
+
+const valida = nome2.length > 0;
+
+valida && console.log(nome2)
+
+const nome3 = "Maria Gabriela";
+
+
